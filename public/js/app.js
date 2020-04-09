96328741550bd394d400bfbeed026caf76b35867
Sehyung Lee <sehyunglee@Sehyung-ui-MacBook-Pro.local>
2020-04-10 07:16:29 +0900

    Setup aap for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ const address =search.value
 
 messageOne.textContent = 'Loading ...'
 messageTwo.textContent = ''
-    fetch('http://localhost:3000/weather?address='+address).then((response)=>{
+    fetch('/weather?address='+address).then((response)=>{
         response.json().then((data)=>{
             if(data.error) {
             messageOne.textContent =data.error
